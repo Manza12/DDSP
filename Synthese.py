95e591d1dc16e838b6d696b173838c279f4b1c11
Manza12 <tritery@hotmail.com>
2019-12-09 19:33:22 +0100

    STFT faite. Il ne manque que la STFT du fichier original et la fonction de cout

--- Synthese.py
+++ Synthese.py
@@ -5,21 +5,37 @@ from Parameters import AUDIO_SAMPLING_RATE, SAMPLE_DURATION, OUTPUT_DIM
 from tqdm import tqdm
 
 
+def synthese_torch(outputs, f0):
+    num_harmonics = OUTPUT_DIM - 1
+    n_sample = AUDIO_SAMPLING_RATE * SAMPLE_DURATION
+    lo = outputs[:, 0]
+    alpha = outputs
+    oldx = np.linspace(0, 1, f0.shape[0])
+    newx = np.linspace(0, 1, n_sample)
+    f0 = np.interp(newx, oldx, f0[:,0]) / AUDIO_SAMPLING_RATE
+    lo = np.interp(newx, oldx, lo.numpy())
+    alpha = np.stack([np.interp(newx, oldx, alpha[:, i]) for i in range(1, num_harmonics+1)])
+    phi = np.zeros(f0.shape)
+    aa = np.zeros((num_harmonics, f0.shape[-1]))
+    k = np.arange(1, num_harmonics + 1).reshape(1, -1)
+    for i in tqdm(np.arange(1, phi.shape[-1])):
+        phi[i] = 2 * np.pi * f0[i] + phi[i - 1]
+        aa[:, i] = (f0[i] * k) < .5
+    y = lo * np.sum(aa * alpha * np.sin(k.reshape(-1, 1) * phi.reshape(1, -1)), 0)
+    wav.write("Test_antoine.wav", AUDIO_SAMPLING_RATE, y)
+    return y
+
+
 def synthese(outputs, f0):
     num_harmonics = OUTPUT_DIM - 1
     n_sample = AUDIO_SAMPLING_RATE * SAMPLE_DURATION
-    stride = AUDIO_SAMPLING_RATE // 100
-    x = np.linspace(0, SAMPLE_DURATION, n_sample // stride)
-    xp = np.linspace(0, SAMPLE_DURATION, n_sample // stride)
-    fp = f0
-    f0 = np.interp(x, xp, fp)
-    lo = np.ones(n_sample//stride)
+    lo = outputs[:, 0]
     alpha = outputs
-    oldx = np.linspace(0, 1, len(f0))
+    oldx = np.linspace(0, 1, f0.shape[0])
     newx = np.linspace(0, 1, n_sample)
-    f0 = np.interp(newx, oldx, f0) / AUDIO_SAMPLING_RATE
-    lo = np.interp(newx, oldx, lo)
-    alpha = np.stack([np.interp(newx, oldx, alpha[:, i]) for i in range(1, num_harmonics+1)])
+    f0 = np.interp(newx, oldx, f0[:,0]) / AUDIO_SAMPLING_RATE
+    lo = np.interp(newx, oldx, lo.detach().numpy())
+    alpha = np.stack([np.interp(newx, oldx, alpha[:, i].detach().numpy()) for i in range(1, num_harmonics+1)])
     phi = np.zeros(f0.shape)
     aa = np.zeros((num_harmonics, f0.shape[-1]))
     k = np.arange(1, num_harmonics + 1).reshape(1, -1)
--- TryNet.py
+++ TryNet.py
@@ -2,8 +2,10 @@ import torch
 
 from NetGon import DDSPNet
 from DataLoader import Dataset
-from Synthese import synthese_harmonique
+from Synthese import synthese
 from torch.utils.data import DataLoader
+from Parameters import STFT_SIZE
+
 
 torch.set_default_tensor_type(torch.DoubleTensor)
 
@@ -18,6 +20,10 @@ for i, data in enumerate(data_loader, 0):
     outputs = net(data)
     f0 = data["frecuency"]
 
-    son = synthese_harmonique(outputs, f0)
-
-    # stft_1 = torch.stft(son)
+    for k in range(outputs.shape[0]):
+        outputs_sample = outputs[k,:,:]
+        f0_sample = f0[k,:,:]
+        son = synthese(outputs_sample, f0_sample)
+        son_tensor = torch.tensor(son)
+        stft = torch.stft(son_tensor, STFT_SIZE, window=torch.hann_window(STFT_SIZE), onesided=True)
+        squared_module = stft[:,:,0]**2 + stft[:,:,1]**2
