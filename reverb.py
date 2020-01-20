db11a2ee37b89d6e3f07f67cea87721f5bca6f9e
Manza12 <tritery@hotmail.com>
2020-01-20 22:14:06 +0100

    PEP8ing reverb

--- reverb.py
+++ reverb.py
@@ -1,12 +1,12 @@
 import torch
-from scipy.io.wavfile import read as wave_read
-from scipy.io.wavfile import write as wave_write
 import torch.nn.functional as func
 
 
 def add_reverb(dry_signal, impulse_response):
     ir_length = impulse_response.shape[-1]
     dry_signal_paded = func.pad(dry_signal, [ir_length//2, ir_length//2])
-    wet_signal = torch.conv1d(dry_signal_paded.unsqueeze(1), impulse_response.unsqueeze(0).unsqueeze(0))
+    wet_signal = torch.conv1d(dry_signal_paded.unsqueeze(1),
+                              impulse_response.unsqueeze(0).unsqueeze(0))
     wet_signal = wet_signal.squeeze(1)
+    
     return 0.3 * wet_signal + 0.7 * dry_signal
