43659592adb5ac34b09197a60af3a6b9873f2e7d
ob <olvb@users.noreply.github.com>
2020-01-21 15:02:25 +0100

    change phase acc computation

--- synthesis.py
+++ synthesis.py
@@ -24,18 +24,27 @@ def synthetize(a0s, f0s, aa, hs, frame_length, sample_rate, device):
     else:
         harm_ranks = torch.arange(nb_harms, device=device) + 1
 
-    ff = f0s.unsqueeze(2) * harm_ranks
 
-    ff = func.interpolate(torch.transpose(ff, 1, 2), size=signal_length,
+    # Multiply f0s by harmonic ranks to get all freqs
+    ff = f0s.unsqueeze(-1) * harm_ranks.unsqueeze(0).unsqueeze(0)
+    # Prevent aliasing
+    max_f = sample_rate / 2.0
+    aa[ff >= max_f] = 0.0
+
+    f0s = f0s.unsqueeze(1)
+    f0s = func.interpolate(f0s, size=signal_length,
                           mode="linear", align_corners=True)
-    ff = torch.transpose(ff, 1, 2)
+    f0s = f0s.squeeze(1)
 
-    # Phase accumulation over time for each freq
-    phases = 2 * np.pi * ff / sample_rate
+    # Phase accumulation over time
+    phases = 2 * np.pi * f0s / sample_rate
     if MODULAR_PHASE_SUM:
         phases_acc = modular_sum(phases, signal_length)
     else:
         phases_acc = torch.cumsum(phases, dim=1)
+
+    # multiply by harmonic ranks to get phase for all harmonics
+    phases_acc = phases_acc.unsqueeze(-1) * harm_ranks
 
     # Normalize amplitudes with a0
     aa_sum = torch.sum(aa, dim=2)
@@ -47,9 +56,6 @@ def synthetize(a0s, f0s, aa, hs, frame_length, sample_rate, device):
     # Interpolate amplitudes
     aa = smoothing_amplitudes(aa, signal_length, HAMMING_WINDOW_LENGTH, device)
 
-    # Prevent aliasing
-    aa[ff >= sample_rate / 2.1] = 0.
-
     # Additive generation
     additive = aa * torch.sin(phases_acc)
 
@@ -57,8 +63,9 @@ def synthetize(a0s, f0s, aa, hs, frame_length, sample_rate, device):
     additive = torch.sum(additive, dim=2)
 
     """ Noise part """
-    noise = filter_noise(create_white_noise(hs.shape[1] * FRAME_LENGTH,
-                                            device=device), hs, device=device)
+    # noise = filter_noise(create_white_noise(hs.shape[1] * FRAME_LENGTH,
+    #                                         device=device), hs, device=device)
+    noise = synthetize_noise(hs,  device=device)
 
     # Empty cache
     torch.cuda.empty_cache()
@@ -81,17 +88,18 @@ def smoothing_amplitudes(aa, signal_length, window_length, device):
 
 def modular_sum(phases, signal_length):
     # Computing phase accumulation modulo 2*pi to minimize error
-    phases_acc = phases
     cursor = 0
     stride = 256
+
     while cursor < signal_length:
-        phases_acc[:, cursor, :] %= (2 * np.pi)
-        phases_acc[:, cursor:cursor + stride, :] = torch.cumsum(
-            phases[:, cursor:cursor + stride, :], dim=1)
+        phases[..., cursor] %= 2.0 * np.pi
+        phases[..., cursor : cursor + stride] = torch.cumsum(
+            phases[..., cursor : cursor + stride], dim=1
+        )
         cursor += stride - 1
+    phases %= 2.0 * np.pi
 
-    return phases_acc
-
+    return phases
 
 def interpolate_hamming(tensor, signal_length, frame_length, device):
     y = torch.zeros((tensor.shape[0], tensor.shape[1] * frame_length,
